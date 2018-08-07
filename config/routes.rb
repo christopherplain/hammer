d23249c92ad66f5407c1ca343149310d90ae3ded
Christopher Plain <christopher@christopherplain.com>
2018-08-06 20:05:38 -0700

    Add StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root to: "rack_configs#index"
+  get 'static_pages/home'
+  get 'static_pages/about'
+  get 'static_pages/help'
 
   resources :parts do
     collection { post :import }
@@ -10,5 +12,7 @@ Rails.application.routes.draw do
   end
 
   post 'rack_configs/:id/import(.:format)', to: 'rack_configs#import', as: 'import_rack_config'
+
+  root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
