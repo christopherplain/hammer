3f966fde3d0b9328115647514c11fa6a3a8bd58c
Christopher Plain <christopher@christopherplain.com>
2018-08-31 10:52:55 -0700

    Add Connection to RackComponent relation

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -12,6 +12,22 @@ class Connection
   field :cable_length, type: String
   embedded_in :interface
 
+  # Create manual relations to RackComponent
+  field :local_device_id, type: BSON::ObjectId
+  field :remote_device_id, type: BSON::ObjectId
+
+  def rack_config
+    self.interface.rack_config
+  end
+
+  def local_device
+    rack_config.elevation.rack_component.find(self.local_device_id) if local_device_id
+  end
+
+  def remote_device
+    rack_config.elevation.rack_component.find(self.remote_device_id) if remote_device_id
+  end
+
   def self.field_keys
     Connection.fields.keys.drop(3)
   end
