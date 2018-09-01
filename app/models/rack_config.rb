b052f3bac4510c7c55bd7832a04cd616133a92f5
Christopher Plain <christopher@christopherplain.com>
2018-08-31 19:23:48 -0700

    Refactor update_rack_component

--- app/models/rack_config.rb
+++ app/models/rack_config.rb
@@ -18,7 +18,7 @@ class RackConfig
       row_hash = row.to_hash
 
       elevation = update_elevation(row_hash, rack_config)
-      component = update_rack_component(row_hash, elevation)
+      RackComponent.update(row_hash, elevation)
     end
 
     # Create/update Interfaces and Connections
@@ -52,29 +52,5 @@ class RackConfig
       elevation.update_attributes!(elevation_hash)
     end
     elevation
-  end
-
-  def self.update_rack_component(row_hash, elevation)
-    # Grab rack component data and search for existing RackComponent.
-    rack_component_hash = row_hash.slice(*RackComponent.field_keys)
-    u_location = rack_component_hash["u_location"].to_i
-    orientation = rack_component_hash["orientation"]
-    part_number = row_hash["part_number"]
-    rack_component = elevation.rack_components.where(
-      u_location: u_location, orientation: orientation).first
-    part = Part.where(part_number: part_number).first
-    if part
-      rack_component_hash[:part_id] = part.id
-    else
-      rack_component_hash[:part_id] = nil
-    end
-
-    # Create new RackComponent or update existing document.
-    if rack_component.nil?
-      rack_component = elevation.rack_components.create!(rack_component_hash)
-    else
-      rack_component.update_attributes!(rack_component_hash)
-    end
-    rack_component
   end
 end
