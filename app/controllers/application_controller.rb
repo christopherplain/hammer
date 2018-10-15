d4c709075538b0d9b306d0a6696d26ac23ef3a04
Christopher Plain <cplain@attunix.com>
2018-10-15 08:56:43 -0700

    Use user layout for devise controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
   before_action :set_customers
+  layout :layout_by_resource
 
-  # Load Customers for navbar links.
-  def set_customers
-    @customers ||= Customer.all.order(name: :asc)
-  end
+  private
+    # Load Customers for navbar links.
+    def set_customers
+      @customers ||= Customer.all.order(name: :asc)
+    end
+
+    def layout_by_resource
+      if devise_controller?
+        "user"
+      else
+        "application"
+      end
+    end
 end
